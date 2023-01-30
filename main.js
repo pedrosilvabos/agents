a3be8a0ada165df12c0046bbe538106cd913639c
Joao Pedro Silva Faria <pedro.silva.bos@gmail.com>
2023-01-30 01:10:00 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -8,43 +8,33 @@ app.get("/", (req, res) => {
 });
 
 app.post("/make-fetch", async (req, res) => {
-  const fetch = require("node-fetch");
-
-  var myHeaders = new Headers();
-  myHeaders.append("Host", " api.geodoge.com");
-  myHeaders.append("Connection", " keep-alive");
-  myHeaders.append("Content-Length", " 15");
-  myHeaders.append(
-    "sec-ch-ua",
-    ' "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"'
-  );
-  myHeaders.append("DNT", " 1");
-  myHeaders.append("sec-ch-ua-mobile", " ?0");
-  myHeaders.append(
-    "User-Agent",
-    " Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
-  );
-  myHeaders.append("Content-Type", " application/x-www-form-urlencoded");
-  myHeaders.append("Lang", " en-us");
-  myHeaders.append("token", "7ef48b12-6671-44b7-89ae-600885eb0697");
-  myHeaders.append("sec-ch-ua-platform", ' "Windows"');
-  myHeaders.append("Accept", " */*");
-  myHeaders.append("Origin", " https://www.amzdoge.com");
-  myHeaders.append("Sec-Fetch-Site", " cross-site");
-  myHeaders.append("Sec-Fetch-Mode", " cors");
-  myHeaders.append("Sec-Fetch-Dest", " empty");
-  myHeaders.append("Referer", " https://www.amzdoge.com/");
-  myHeaders.append("Accept-Encoding", " gzip, deflate, br");
-  myHeaders.append("Accept-Language", " en-US,en;q=0.9,pt;q=0.8");
-  myHeaders.append(
-    "Cookie",
-    "aliyungf_tc=ca58e39959ea41c18df09aed477bbc7b93c3af6dc0814212af16429b5bc69b7b"
-  );
+  const headers = `
+    Host: api.geodoge.com
+    Connection: keep-alive
+    Content-Length: 15
+    sec-ch-ua: "Not_A Brand";v="99", "Google Chrome";v="109", "Chromium";v="109"
+    DNT: 1
+    sec-ch-ua-mobile: ?0
+    User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36
+    Content-Type: application/x-www-form-urlencoded
+    Lang: en-us
+    token: 7ef48b12-6671-44b7-89ae-600885eb0697
+    sec-ch-ua-platform: Windows
+    Accept: */*
+    Origin: https://www.amzdoge.com
+    Sec-Fetch-Site: cross-site
+    Sec-Fetch-Mode: cors
+    Sec-Fetch-Dest: empty
+    Referer: https://www.amzdoge.com/
+    Accept-Encoding: gzip, deflate, br
+    Accept-Language: en-US,en;q=0.9,pt;q=0.8
+    Cookie: aliyungf_tc=ca58e39959ea41c18df09aed477bbc7b93c3af6dc0814212af16429b5bc69b7b
+  `;
 
   (async () => {
     const response = await fetch("https://api.geodoge.com/api/order/speedup", {
       method: "POST",
-      headers: myHeaders,
+      headers: headers,
       body:
         "orderid=" +
         Math.floor(Math.random() * (1181788 - 1131788 + 1)) +
