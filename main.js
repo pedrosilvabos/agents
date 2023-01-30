8d61ab75082a329043ba395b05efbcafd82f3315
Joao Pedro Silva Faria <pedro.silva.bos@gmail.com>
2023-01-30 01:05:47 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -56,6 +56,6 @@ app.post("/make-fetch", async (req, res) => {
   })();
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server running on port 3000");
 });
